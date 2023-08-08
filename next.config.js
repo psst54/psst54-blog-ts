49fe5d62a614dc22d17c6ba86f7b6a513849dc3a
psst54 <psst5491@naver.com>
2023-08-09 08:14:39 +0900

    🔧 set language to korean

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko",
+  },
 };
 
 module.exports = withContentlayer(nextConfig);

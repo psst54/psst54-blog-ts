8692c1b92ae91dc7fb87593ab1017315d9e274c0
psst54 <psst5491@naver.com>
2023-08-09 08:23:41 +0900

    🐛 fix deploy error

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ["ko"],
-    defaultLocale: "ko",
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
